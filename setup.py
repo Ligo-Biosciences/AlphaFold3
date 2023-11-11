e0ac61f42da58386a219a093a30072013bc2f4f3
Arda <98056717+ardagoreci@users.noreply.github.com>
2023-11-11 12:42:58 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Describe Your Cool Project",
+    author="",
+    author_email="",
+    url="https://github.com/user/project",
+    install_requires=["lightning", "hydra-core"],
+    packages=find_packages(),
+    # use this to customize global commands available in the terminal after installing the package
+    entry_points={
+        "console_scripts": [
+            "train_command = src.train:main",
+            "eval_command = src.eval:main",
+        ]
+    },
+)
