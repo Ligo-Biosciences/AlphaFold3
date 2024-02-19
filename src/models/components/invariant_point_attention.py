dadc90609cb244d0efe3170d684e03ef716849bf
ardagoreci <62720042+ardagoreci@users.noreply.github.com>
2024-02-19 16:47:56 +0000

    Refactored IPA

--- src/models/components/invariant_point_attention.py
+++ src/models/components/invariant_point_attention.py
@@ -14,13 +14,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Implements the Invariant Point Attention module."""
 
-from functools import reduce
+# TODO: I am not happy with the cryptic way this module has been written. I will
+#  refactor it to make it more readable.
+
 import math
-import sys
-from operator import mul
 import torch
 import torch.nn as nn
+from torch.utils.checkpoint import checkpoint
 from typing import Optional, Tuple, Sequence
 
 from src.utils.precision_utils import is_fp16_enabled
@@ -75,6 +77,10 @@ class InvariantPointAttention(nn.Module):
         self.inf = inf
         self.eps = eps
 
+        # Constants used for scaling the attention weights
+        self.scale_L = math.sqrt(1 / 3)
+        self.scale_C = math.sqrt(2 / (9 * no_qk_points))
+
         # These linear layers differ from their specifications in the
         # supplement. There, they lack bias and use Glorot initialization.
         # Here as in the official source, they have bias and use the default
@@ -89,8 +95,6 @@ class InvariantPointAttention(nn.Module):
         hpkv = self.no_heads * (self.no_qk_points + self.no_v_points) * 3
         self.linear_kv_points = Linear(self.c_s, hpkv)
 
-        hpv = self.no_heads * self.no_v_points * 3
-
         self.linear_b = Linear(self.c_z, self.no_heads)
 
         self.head_weights = nn.Parameter(torch.zeros(no_heads))
@@ -104,15 +108,72 @@ class InvariantPointAttention(nn.Module):
         self.softmax = nn.Softmax(dim=-1)
         self.softplus = nn.Softplus()
 
-    def forward(
+    def compute_attention_scores(
+            self,
+            z: torch.Tensor,  # [*, N_res, N_res, C_z]
+            q: torch.Tensor,  #
+            k: torch.Tensor,  #
+            q_pts: torch.Tensor,  #
+            k_pts: torch.Tensor,  #
+            mask: torch.Tensor) -> torch.Tensor:
+
+        # Compute Bias of shape [*, N_res, N_res, H]
+        b = self.linear_b(z)
+
+        # QK tensor [*, H, N_res, N_res]
+        if is_fp16_enabled():
+            with torch.cuda.amp.autocast(enabled=False):
+                qk = torch.matmul(
+                    permute_final_dims(q.float(), (1, 0, 2)),  # [*, H, N_res, C_hidden]
+                    permute_final_dims(k.float(), (1, 2, 0)),  # [*, H, C_hidden, N_res]
+                )
+        else:
+            qk = torch.matmul(
+                permute_final_dims(q, (1, 0, 2)),  # [*, H, N_res, C_hidden]
+                permute_final_dims(k, (1, 2, 0)),  # [*, H, C_hidden, N_res]
+            )
+        qk_scaled = qk * math.sqrt(1 / self.c_hidden)  # scaled qk tensor
+        qk_scaled_plus_b = qk_scaled + permute_final_dims(b, (2, 0, 1))
+
+        # Compute point attention affinities based on the query and key points
+        # [*, N_res, N_res, H, P_q, 3]
+        pt_att_squared_diff = (q_pts.unsqueeze(-4) - k_pts.unsqueeze(-5)) ** 2
+        # [*, N_res, N_res, H, P_q]
+        pt_att_affinities = torch.sum(pt_att_squared_diff, dim=-1)
+
+        # Compute head weights as the softplus of a learnable scalar
+        head_weights = self.softplus(self.head_weights).view(
+            *((1,) * len(pt_att_affinities.shape[:-2]) + (-1, 1))  # TODO: cryptic!
+        )
+
+        # Scale the point attention affinities with head weights and constant
+        pt_att = (- head_weights * self.scale_C / 2) * pt_att_affinities
+
+        # Aggregate the point attention affinities over the query points
+        # [*, N_res, N_res, H]
+        pt_att = torch.sum(pt_att, dim=-1)
+
+        # [*, H, N_res, N_res]
+        pt_att = permute_final_dims(pt_att, (2, 0, 1))
+
+        # Compute square mask to be applied of shape [*, N_res, N_res]
+        square_mask = mask.unsqueeze(-1) * mask.unsqueeze(-2)
+        square_mask = self.inf * (square_mask - 1)
+
+        # Compute affinities and mask them
+        affinities = self.scale_L * (qk_scaled_plus_b + pt_att)
+        affinities = affinities + square_mask.unsqueeze(-3)
+
+        # Compute attention scores over j
+        attention = self.softmax(affinities)
+        return attention
+
+    def forward_pass(
             self,
             s: torch.Tensor,
-            z: Optional[torch.Tensor],
+            z: torch.Tensor,
             r: Rigids,
             mask: torch.Tensor,
-            inplace_safe: bool = False,
-            _offload_inference: bool = False,
-            _z_reference_list: Optional[Sequence[torch.Tensor]] = None,
     ) -> torch.Tensor:
         """
         Args:
@@ -127,10 +188,133 @@ class InvariantPointAttention(nn.Module):
         Returns:
             [*, N_res, C_s] single representation update
         """
-        if _offload_inference and inplace_safe:
-            z = _z_reference_list
-        else:
-            z = [z]
+        z = [z]
+
+        #######################################
+        # Generate scalar and point activations
+        #######################################
+        # [*, N_res, H * C_hidden]
+        q = self.linear_q(s)
+        kv = self.linear_kv(s)
+
+        # [*, N_res, H, C_hidden]
+        q = q.view(q.shape[:-1] + (self.no_heads, -1))
+
+        # [*, N_res, H, 2 * C_hidden]
+        kv = kv.view(kv.shape[:-1] + (self.no_heads, -1))
+
+        # [*, N_res, H, C_hidden]
+        k, v = torch.split(kv, self.c_hidden, dim=-1)
+
+        # [*, N_res, H * P_q * 3]
+        q_pts = self.linear_q_points(s)
+
+        # This is kind of clunky, but it's how the original does it
+        # [*, N_res, H * P_q, 3]
+        q_pts = torch.split(q_pts, q_pts.shape[-1] // 3, dim=-1)
+        q_pts = torch.stack(q_pts, dim=-1)
+        q_pts = r[..., None].apply(q_pts)
+
+        # [*, N_res, H, P_q, 3]
+        q_pts = q_pts.view(
+            q_pts.shape[:-2] + (self.no_heads, self.no_qk_points, 3)
+        )
+
+        # [*, N_res, H * (P_q + P_v) * 3]
+        kv_pts = self.linear_kv_points(s)
+
+        # [*, N_res, H * (P_q + P_v), 3]
+        kv_pts = torch.split(kv_pts, kv_pts.shape[-1] // 3, dim=-1)
+        kv_pts = torch.stack(kv_pts, dim=-1)
+        kv_pts = r[..., None].apply(kv_pts)
+
+        # [*, N_res, H, (P_q + P_v), 3]
+        kv_pts = kv_pts.view(kv_pts.shape[:-2] + (self.no_heads, -1, 3))
+
+        # [*, N_res, H, P_q/P_v, 3]
+        k_pts, v_pts = torch.split(
+            kv_pts, [self.no_qk_points, self.no_v_points], dim=-2
+        )
+
+        # Compute attention scores with grad checkpointing
+        # [*, N_res, N_res, H]
+        attention = checkpoint(self.compute_attention_scores, z[0], q, k, q_pts, k_pts, mask)
+
+        ################
+        # Compute output
+        ################
+        # [*, N_res, H, C_hidden]
+        o = torch.matmul(
+            attention, v.transpose(-2, -3).to(dtype=attention.dtype)
+        ).transpose(-2, -3)
+
+        # [*, N_res, H * C_hidden]
+        o = flatten_final_dims(o, 2)
+
+        # [*, H, 3, N_res, P_v]
+        o_pt = torch.sum(
+            (
+                    attention[..., None, :, :, None]
+                    * permute_final_dims(v_pts, (1, 3, 0, 2))[..., None, :, :]
+            ),
+            dim=-2,
+        )
+
+        # [*, N_res, H, P_v, 3]
+        o_pt = permute_final_dims(o_pt, (2, 0, 3, 1))
+        o_pt = r[..., None, None].invert_apply(o_pt)
+
+        # [*, N_res, H * P_v]
+        o_pt_norm = flatten_final_dims(
+            torch.sqrt(torch.sum(o_pt ** 2, dim=-1) + self.eps), 2
+        )
+
+        # [*, N_res, H * P_v, 3]
+        o_pt = o_pt.reshape(*o_pt.shape[:-3], -1, 3)
+
+        # [*, N_res, H, C_z]
+        o_pair = torch.matmul(attention.transpose(-2, -3), z[0].to(dtype=attention.dtype))
+
+        # [*, N_res, H * C_z]
+        o_pair = flatten_final_dims(o_pair, 2)
+
+        # [*, N_res, C_s]
+        s = self.linear_out(
+            torch.cat(
+                (o, *torch.unbind(o_pt, dim=-1), o_pt_norm, o_pair), dim=-1
+            ).to(dtype=z[0].dtype)
+        )
+
+        return s
+
+    def forward(self,
+                s: torch.Tensor,
+                z: Optional[torch.Tensor],
+                r: Rigids,
+                mask: torch.Tensor) -> torch.Tensor:
+        return self.forward_pass(s, z, r, mask)
+
+    def forward_original(
+            self,
+            s: torch.Tensor,
+            z: Optional[torch.Tensor],
+            r: Rigids,
+            mask: torch.Tensor,
+    ) -> torch.Tensor:
+        """
+            Args:
+                s:
+                    [*, N_res, C_s] single representation
+                z:
+                    [*, N_res, N_res, C_z] pair representation
+                r:
+                    [*, N_res] transformation object
+                mask:
+                    [*, N_res] mask
+            Returns:
+                [*, N_res, C_s] single representation update
+            """
+        z = [z]
 
         #######################################
         # Generate scalar and point activations
@@ -184,10 +368,6 @@ class InvariantPointAttention(nn.Module):
         # [*, N_res, N_res, H]
         b = self.linear_b(z[0])
 
-        if _offload_inference:
-            assert (sys.getrefcount(z[0]) == 2)
-            z[0] = z[0].cpu()
-
         # [*, H, N_res, N_res]
         if is_fp16_enabled():
             with torch.cuda.amp.autocast(enabled=False):
@@ -206,10 +386,8 @@ class InvariantPointAttention(nn.Module):
 
         # [*, N_res, N_res, H, P_q, 3]
         pt_att = q_pts.unsqueeze(-4) - k_pts.unsqueeze(-5)
-        if inplace_safe:
-            pt_att *= pt_att
-        else:
-            pt_att = pt_att ** 2
+
+        pt_att = pt_att ** 2
 
         # [*, N_res, N_res, H, P_q]
         pt_att = sum(torch.unbind(pt_att, dim=-1))
@@ -219,10 +397,8 @@ class InvariantPointAttention(nn.Module):
         head_weights = head_weights * math.sqrt(
             1.0 / (3 * (self.no_qk_points * 9.0 / 2))
         )
-        if inplace_safe:
-            pt_att *= head_weights
-        else:
-            pt_att = pt_att * head_weights
+
+        pt_att = pt_att * head_weights
 
         # [*, N_res, N_res, H]
         pt_att = torch.sum(pt_att, dim=-1) * (-0.5)
@@ -233,20 +409,9 @@ class InvariantPointAttention(nn.Module):
         # [*, H, N_res, N_res]
         pt_att = permute_final_dims(pt_att, (2, 0, 1))
 
-        if inplace_safe:
-            a += pt_att
-            del pt_att
-            a += square_mask.unsqueeze(-3)
-            # in-place softmax
-            attn_core_inplace_cuda.forward_(
-                a,
-                reduce(mul, a.shape[:-1]),
-                a.shape[-1],
-            )
-        else:
-            a = a + pt_att
-            a = a + square_mask.unsqueeze(-3)
-            a = self.softmax(a)
+        a = a + pt_att
+        a = a + square_mask.unsqueeze(-3)
+        a = self.softmax(a)
 
         ################
         # Compute output
@@ -260,21 +425,14 @@ class InvariantPointAttention(nn.Module):
         o = flatten_final_dims(o, 2)
 
         # [*, H, 3, N_res, P_v]
-        if inplace_safe:
-            v_pts = permute_final_dims(v_pts, (1, 3, 0, 2))
-            o_pt = [
-                torch.matmul(a, v.to(a.dtype))
-                for v in torch.unbind(v_pts, dim=-3)
-            ]
-            o_pt = torch.stack(o_pt, dim=-3)
-        else:
-            o_pt = torch.sum(
-                (
-                        a[..., None, :, :, None]
-                        * permute_final_dims(v_pts, (1, 3, 0, 2))[..., None, :, :]
-                ),
-                dim=-2,
-            )
+
+        o_pt = torch.sum(
+            (
+                    a[..., None, :, :, None]
+                    * permute_final_dims(v_pts, (1, 3, 0, 2))[..., None, :, :]
+            ),
+            dim=-2,
+        )
 
         # [*, N_res, H, P_v, 3]
         o_pt = permute_final_dims(o_pt, (2, 0, 3, 1))
@@ -287,9 +445,6 @@ class InvariantPointAttention(nn.Module):
 
         # [*, N_res, H * P_v, 3]
         o_pt = o_pt.reshape(*o_pt.shape[:-3], -1, 3)
-
-        if _offload_inference:
-            z[0] = z[0].to(o_pt.device)
 
         # [*, N_res, H, C_z]
         o_pair = torch.matmul(a.transpose(-2, -3), z[0].to(dtype=a.dtype))
