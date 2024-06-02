5d2fb4a608ac269a28f577b94e7265cf4d33c129
ardagoreci <62720042+ardagoreci@users.noreply.github.com>
2024-06-02 15:08:11 +0100

    Implemented alignment methods

--- src/utils/geometry/alignment.py
+++ src/utils/geometry/alignment.py
@@ -1,4 +1,4 @@
-"""There is a weighted rigid alignment of the ground truth onto the denoised structure before
+"""Methods for weighted rigid alignment of the ground truth onto the denoised structure before
 the diffusion loss is applied."""
 import torch
 from src.utils.geometry.vector import Vec3Array
