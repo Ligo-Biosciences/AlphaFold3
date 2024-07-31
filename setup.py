c6fdb692c795359d342fb0a8afc2aa7b112ff8fd
ardagoreci <62720042+ardagoreci@users.noreply.github.com>
2024-07-31 11:36:01 -0700

    Revert to OF setup

--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
-#!/usr/bin/env python
+# Copyright 2021 AlQuraishi Laboratory
+# Copyright 2021 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os
 from setuptools import setup, Extension, find_packages
 import subprocess
+
 import torch
 from torch.utils.cpp_extension import BuildExtension, CppExtension, CUDAExtension, CUDA_HOME
+
 from scripts.utils import get_nvidia_cc
+
+
 version_dependent_macros = [
     '-DVERSION_GE_1_1',
     '-DVERSION_GE_1_3',
     '-DVERSION_GE_1_5',
 ]
+
 extra_cuda_flags = [
     '-std=c++14',
     '-maxrregcount=50',
@@ -21,7 +39,7 @@ extra_cuda_flags = [
 
 
 def get_cuda_bare_metal_version(cuda_dir):
-    if cuda_dir == None or torch.version.cuda == None:
+    if cuda_dir is None or torch.version.cuda is None:
         print("CUDA is not found, cpu version is installed")
         return None, -1, 0
     else:
@@ -31,42 +49,37 @@ def get_cuda_bare_metal_version(cuda_dir):
         release = output[release_idx].split(".")
         bare_metal_major = release[0]
         bare_metal_minor = release[1][0]
+        
         return raw_output, bare_metal_major, bare_metal_minor
 
 
-# Add this new function to run the setup.py in flash-attn-with-bias
-def run_flash_attn_setup():
-    flash_attn_dir = os.path.join('csrc', 'flash-attn-with-bias')
-    if os.path.exists(os.path.join(flash_attn_dir, 'setup.py')):
-        current_dir = os.getcwd()
-        os.chdir(flash_attn_dir)
-        subprocess.check_call(['python', 'setup.py', 'build_ext', '--inplace'])
-        os.chdir(current_dir)
-    else:
-        print("Warning: setup.py not found in flash-attn-with-bias directory")
-
-
-compute_capabilities = {
-    (3, 7),  # K80, e.g.
-    (5, 2),  # Titan X
-    (6, 1),  # GeForce 1000-series
-}
+compute_capabilities = set([
+    (3, 7), # K80, e.g.
+    (5, 2), # Titan X
+    (6, 1), # GeForce 1000-series
+])
 
 
 compute_capabilities.add((7, 0))
 _, bare_metal_major, _ = get_cuda_bare_metal_version(CUDA_HOME)
 if int(bare_metal_major) >= 11:
     compute_capabilities.add((8, 0))
+
 compute_capability, _ = get_nvidia_cc()
 if compute_capability is not None:
-    compute_capabilities = {compute_capability}
+    compute_capabilities = set([compute_capability])
+
 cc_flag = []
 for major, minor in list(compute_capabilities):
     cc_flag.extend([
         '-gencode',
         f'arch=compute_{major}{minor},code=sm_{major}{minor}',
     ])
+
 extra_cuda_flags += cc_flag
+
+cc_flag = ['-gencode', 'arch=compute_70,code=sm_70']
+
 if bare_metal_major != -1:
     modules = [CUDAExtension(
         name="attn_core_inplace_cuda",
@@ -83,9 +96,9 @@ if bare_metal_major != -1:
         extra_compile_args={
             'cxx': ['-O3'] + version_dependent_macros,
             'nvcc': (
-                    ['-O3', '--use_fast_math'] +
-                    version_dependent_macros +
-                    extra_cuda_flags
+                ['-O3', '--use_fast_math'] +
+                version_dependent_macros +
+                extra_cuda_flags
             ),
         }
     )]
@@ -100,25 +113,27 @@ else:
             'cxx': ['-O3'],
         }
     )]
+
 setup(
-    name="src",
-    version="0.0.1",
-    description="Open source implementation of DeepMind's AlphaFold3",
-    author="Arda Goreci",
-    author_email="arda@ligo.bio",
-    url="https://github.com/Ligo-Biosciences/AlphaFold3",
-    install_requires=["lightning", "hydra-core"],
-    packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "train_command = src.train:main",
-            "eval_command = src.eval:main",
-        ]
+    name='openfold',
+    version='2.0.0',
+    description='A PyTorch reimplementation of DeepMind\'s AlphaFold 2',
+    author='OpenFold Team',
+    author_email='jennifer.wei@omsf.io',
+    license='Apache License, Version 2.0',
+    url='https://github.com/aqlaboratory/openfold',
+    packages=find_packages(exclude=["tests", "scripts"]),
+    include_package_data=True,
+    package_data={
+        "openfold": ['utils/kernel/csrc/*'],
+        "": ["resources/stereo_chemical_props.txt"]
     },
     ext_modules=modules,
-    cmdclass={
-        'build_ext': BuildExtension.with_options(use_ninja=False)
-    },
+    cmdclass={'build_ext': BuildExtension},
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.9,'
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
 )
-# Call the function to run flash-attn-with-bias setup
-run_flash_attn_setup()
