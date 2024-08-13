41ad3dfdab4b58a17f859eedfe5849423344f823
ardagoreci <62720042+ardagoreci@users.noreply.github.com>
2024-08-13 03:03:08 -0700

    disable templates

--- src/models/model.py
+++ src/models/model.py
@@ -22,9 +22,9 @@ class AlphaFold3(nn.Module):
             **self.config["input_embedder"]
         )
 
-        self.template_embedder = TemplateEmbedder(
-            **self.config["template_embedder"]
-        )
+        # self.template_embedder = TemplateEmbedder(
+        #    **self.config["template_embedder"]
+        # )
 
         self.msa_module = MSAModule(
             **self.config["msa_module"]
@@ -107,17 +107,17 @@ class AlphaFold3(nn.Module):
         del s_prev, z_prev, s_init, z_init
 
         # Embed the templates
-        z = add(z,
-                self.template_embedder(
-                    feats,
-                    z,
-                    pair_mask=pair_mask,
-                    chunk_size=self.globals.chunk_size,
-                    use_deepspeed_evo_attention=self.globals.use_deepspeed_evo_attention,
-                    inplace_safe=inplace_safe
-                ),
-                inplace=inplace_safe
-                )
+        # z = add(z,
+        #        self.template_embedder(
+        #            feats,
+        #            z,
+        #            pair_mask=pair_mask,
+        #            chunk_size=self.globals.chunk_size,
+        #            use_deepspeed_evo_attention=self.globals.use_deepspeed_evo_attention,
+        #            inplace_safe=inplace_safe
+        #        ),
+        #        inplace=inplace_safe
+        #        )
 
         # Process the MSA
         z = add(z,
