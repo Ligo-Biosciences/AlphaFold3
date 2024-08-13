7bd3f7338139aad2e13bd5eff58f3eb38e3a4b31
ardagoreci <62720042+ardagoreci@users.noreply.github.com>
2024-08-12 19:53:24 -0700

    added weighted alignment for the loss

--- src/utils/loss.py
+++ src/utils/loss.py
@@ -109,9 +109,8 @@ def diffusion_loss(
     pred_atoms = Vec3Array.from_array(pred_atoms)
     gt_atoms = Vec3Array.from_array(gt_atoms)
 
-    # Align the gt_atoms to pred_atoms  TODO: add the alignment back in
-    aligned_gt_atoms = gt_atoms
-    # weighted_rigid_align(x=gt_atoms, x_gt=pred_atoms, weights=weights, mask=mask))
+    # Align the gt_atoms to pred_atoms
+    aligned_gt_atoms = weighted_rigid_align(x=gt_atoms, x_gt=pred_atoms, weights=weights, mask=mask)
 
     # MSE loss
     mse = mean_squared_error(pred_atoms, aligned_gt_atoms, weights, mask)
