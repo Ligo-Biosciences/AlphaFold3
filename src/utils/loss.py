573571bfed9e86d30505b9945181e06d122eed0a
ardagoreci <62720042+ardagoreci@users.noreply.github.com>
2024-08-30 16:49:06 -0700

    increased weight decay, reduce lr

--- src/utils/loss.py
+++ src/utils/loss.py
@@ -147,7 +147,7 @@ def mse_loss(
     mse = mean_squared_error(pred_atoms, aligned_gt_atoms, weights, mask)
 
     # Scale by (t**2 + σ**2) / (t + σ)**2
-    scaling_factor = (timesteps ** 2 + sd_data ** 2) / ((timesteps + sd_data) ** 2 + epsilon)
+    scaling_factor = (timesteps ** 2 + sd_data ** 2) / ((timesteps * sd_data) ** 2 + epsilon)
     scaled_mse = scaling_factor.squeeze(-1) * mse  # (bs,)
 
     # Average over batch dimension
