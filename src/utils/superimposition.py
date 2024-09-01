ccac3238931d1a9533c16a6086fb4e73656e3109
ardagoreci <62720042+ardagoreci@users.noreply.github.com>
2024-08-31 17:53:27 -0700

    bug fixes

--- src/utils/superimposition.py
+++ src/utils/superimposition.py
@@ -3,12 +3,20 @@ from src.utils.geometry.vector import Vec3Array
 import torch
 
 
-def compute_rmsd(tensor1, tensor2, eps=1e-6):
+def compute_rmsd(tensor1, tensor2, mask, eps=1e-6):
     """Compute the RMSD between two tensors."""
     diff = tensor1 - tensor2
     squared_diff = diff ** 2
     sum_squared_diff = squared_diff.sum(dim=-1)
-    mean_squared_diff = sum_squared_diff.mean(dim=-1)
+
+    # Mask out invalid positions
+    sum_squared_diff = sum_squared_diff * mask
+
+    # Average over valid positions
+    denom = mask.sum(dim=-1) + eps
+    mean_squared_diff = torch.sum(sum_squared_diff, dim=-1) / denom
+
+    # Square root to get RMSD
     rmsd = torch.sqrt(mean_squared_diff + eps)
     return rmsd
 
